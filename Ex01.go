2c8d9a39037faa08e2d73c1559b9ddecccbbfd3b
felpsbc <fbcarvalho2004@gmail.com>
2023-09-04 18:14:59 -0300

    Exercicio

---
+++ Ex01.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var numeros [3]int
+	fmt.Println("Escreva 3 números ")
+	fmt.Scan(&numeros[0], &numeros[1], &numeros[2])
+
+	var soma int
+	soma = numeros[0] + numeros[1] + numeros[2]
+
+	fmt.Printf("A soma dos números do Array é: %d", soma)
+
+}
