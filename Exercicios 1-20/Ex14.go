2a2856f2918f29f8f7ab3721232debb3e80df83b
felpsbc <fbcarvalho2004@gmail.com>
2023-09-18 15:13:34 -0300

    exercicios

--- Exercicios 1-20/Ex14.go
+++ Exercicios 1-20/Ex14.go
@@ -4,14 +4,18 @@ import "fmt"
 
 func main() {
 
-	Slice := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8}
 
 	var indice1, indice2 int
+	fmt.Println(slice)
 	fmt.Println("Escreva os índices dos números que deverão ser trocados de posição ")
 	fmt.Scan(&indice1, &indice2)
 
-	if indice1 < 0 || indice1 > 7 || indice2 < 0 || indice2 > 7 {
-		fmt.Println("Indices invalidos")
-		return
+	if indice1 != indice2 {
+		slice[indice1], slice[indice2] = slice[indice2], slice[indice1]
+		fmt.Println("Novo Slice :", slice)
+	} else {
+		fmt.Println("Os indices são os mesmos então continua igual.", slice)
 	}
+
 }
